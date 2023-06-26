95e343d12d2fe5ab79e4f65655c2f01c239f51e9
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-26 16:26:07 -0700

    Added and implemented Invoices page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/Team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/global/bar";
 // import Form from "./scenes/global/form";
@@ -30,7 +30,7 @@ function App() {
                     <Route path="/" element={<Dashboard />} /> 
                     <Route path="/team" element={<Team />} /> 
                     <Route path="/contacts" element={<Contacts />} /> 
-                    {/* <Route path="/invoices" element={<Invoices />} />  */}
+                    <Route path="/invoices" element={<Invoices />} /> 
                     {/* <Route path="/form" element={<Form />} />  */}
                     {/* <Route path="/bar" element={<Bar />} />  */}
                     {/* <Route path="/pie" element={<Pie />} />  */}
