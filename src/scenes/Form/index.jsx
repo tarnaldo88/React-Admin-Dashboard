5b20b0fb333a669793104bb6d823367b5e1bc438
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-26 16:40:47 -0700

    added Regex for form

--- src/scenes/Form/index.jsx
+++ src/scenes/Form/index.jsx
@@ -5,6 +5,26 @@ import { tokens } from "../../theme";
 import useMediaQuery from "@mui/material";
 import Header from "../../components/Header";
 
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: "",
+};
+
+const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("Invalid Email").required("required"),
+    contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
+    address1: yup.string().required("required"),
+    address2: yup.string().required("required"),
+})
+
 const Form = () =>{
     const isNonMobile = useMediaQuery("(min-widthL:600px)");
 
@@ -19,8 +39,9 @@ const Form = () =>{
             <Formik 
                 onSubmit={handleFormSubmit}
                 initialValues={initialValues}
+                validationSchema={userSchema}
             >
-                
+
             </Formik>
 
         </Box>
