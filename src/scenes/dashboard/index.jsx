4b3116132d349d6e01a4571990c04ccbf930e4c7
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-27 11:55:00 -0700

    fixed import in statbox and progressbox, and added imports to dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,16 @@
-import { Box } from "@mui/material";
+import { Box, useTheme, Typography, IconButton, Button } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import {mockTransctions} from "../../data/mockData";
+import { DownloadOutlinedIcon } from "@mui/icons-material/DownloadOutlined";
+import { EmailIcon } from "@mui/icons-material/Email";
+import { PointOfSaleIcon } from "@mui/icons-material/PointOfSale";
+import { PersonAddIcon } from "@mui/icons-material/PersonAdd";
+import { TrafficIcon } from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import GeographyChart from "../../components/GeographyChart";
+import StatBox from "../../components/StatBox";
 
 const Dashboard = () => {
     return (<Box m="20px">
