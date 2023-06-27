5e683e6c10c2486de6ec515d139bf270a4c32476
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-27 11:35:40 -0700

    Geography chart formatted

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,12 +1,17 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import GeoChart from "../../components/GeographyChart";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 
 const Geography= ()=> {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    
     return(
         <Box m="20px">
             <Header title="Geography Chart" subtitle="A Geography Chart from nivo" />
-            <Box height="75vh">
+            <Box height="75vh" border= {`1px solid ${colors.grey[100]}`}>
                 <GeoChart/>
             </Box>
         </Box>
