85aea6381f6ceb41fc077e0735ee302b276ebbb6
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-26 17:12:11 -0700

    Form updated and working

--- src/scenes/Form/index.jsx
+++ src/scenes/Form/index.jsx
@@ -2,7 +2,7 @@ import { Box,Button, TextField, Typography, useTheme } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { tokens } from "../../theme";
-import useMediaQuery from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
 const initialValues = {
@@ -58,9 +58,74 @@ const Form = () =>{
                                 onChange={handleChange} 
                                 value={values.firstName} 
                                 name="firstName" 
-                                error={!!touched.firstName && !!error.firstName} 
+                                error={!!touched.firstName && !!errors.firstName} 
                                 helperText={touched.firstName && errors.firstName}
                                 sx = {{gridColumn: "span 2"}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                variant="filled" 
+                                type="text" 
+                                label="Last Name" 
+                                onBlur={handleBlur} 
+                                onChange={handleChange} 
+                                value={values.lastName} 
+                                name="lastName" 
+                                error={!!touched.lastName && !!errors.lastName} 
+                                helperText={touched.lastName && errors.lastName}
+                                sx = {{gridColumn: "span 2"}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                variant="filled" 
+                                type="text" 
+                                label="Email" 
+                                onBlur={handleBlur} 
+                                onChange={handleChange} 
+                                value={values.email} 
+                                name="email" 
+                                error={!!touched.email && !!errors.email} 
+                                helperText={touched.email && errors.email}
+                                sx = {{gridColumn: "span 4"}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                variant="filled" 
+                                type="text" 
+                                label="Contact Number" 
+                                onBlur={handleBlur} 
+                                onChange={handleChange} 
+                                value={values.contact} 
+                                name="contact" 
+                                error={!!touched.contact && !!errors.contact} 
+                                helperText={touched.contact && errors.contact}
+                                sx = {{gridColumn: "span 4"}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                variant="filled" 
+                                type="text" 
+                                label="Address" 
+                                onBlur={handleBlur} 
+                                onChange={handleChange} 
+                                value={values.address1} 
+                                name="address1" 
+                                error={!!touched.address1 && !!errors.address1} 
+                                helperText={touched.address1 && errors.address1}
+                                sx = {{gridColumn: "span 4"}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                variant="filled" 
+                                type="text" 
+                                label="Address" 
+                                onBlur={handleBlur} 
+                                onChange={handleChange} 
+                                value={values.address2} 
+                                name="address2" 
+                                error={!!touched.address2 && !!errors.address2} 
+                                helperText={touched.address2 && errors.addres2}
+                                sx = {{gridColumn: "span 4"}}
                             />
                         </Box>
                     </form>
