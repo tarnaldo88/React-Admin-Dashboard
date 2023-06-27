f74c7424a981a427c8c13126d96be59154fa0a2e
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-27 12:09:57 -0700

    fixed imports and added download button

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,22 +1,44 @@
 import { Box, useTheme, Typography, IconButton, Button } from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
-import {mockTransctions} from "../../data/mockData";
-import { DownloadOutlinedIcon } from "@mui/icons-material/DownloadOutlined";
-import { EmailIcon } from "@mui/icons-material/Email";
-import { PointOfSaleIcon } from "@mui/icons-material/PointOfSale";
-import { PersonAddIcon } from "@mui/icons-material/PersonAdd";
-import { TrafficIcon } from "@mui/icons-material/Traffic";
+import {mockTransactions} from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
 import LineChart from "../../components/LineChart";
 import BarChart from "../../components/BarChart";
 import GeographyChart from "../../components/GeographyChart";
 import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (<Box m="20px">
             <Box display ="flex" justifyContent="space-between" alignItems={"center"}>
                 <Header title="DASHBOARD" subtitle="Welcome to the Dashboard"/>
             </Box>
+
+            <Box>
+                <Button
+                    sx={{
+                        backgroundColor: colors.blueAccent[700],
+                        color: colors.grey[100],
+                        fontSize: "14px",
+                        fontWeight:"bold",
+                        padding:"10px 20px"
+                    }}
+                >
+                    <DownloadOutlinedIcon sx= {{ mr: "10px"}} />
+                    Download Reports
+                </Button>
+            </Box>
+
+            {/* Grid and Charts */}
+            
         </Box>
     );
 }
