a41909c5887eb1aa36752a245ac8567a93ce2e7d
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-26 20:52:32 -0700

    Geo chart formatting complete

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -4,7 +4,7 @@ import { mockGeographyData as data} from "../data/mockData";
 import {geoFeatures} from "../data/mockGeoFeatures";
 import { tokens } from "../theme";
 
-const GeoChart = () => {
+const GeoChart = ({isDashboard = false} ) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
@@ -38,72 +38,18 @@ const GeoChart = () => {
         }}
         features={geoFeatures.features}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors="nivo"
         domain={[ 0, 1000000 ]}
         unknownColor="#666666"
         label="properties.name"
         valueFormat=".2s"
-        projectionTranslation={[ 0.5, 0.5 ]}
-        projectionRotation={[ 0, 0, 0 ]}
-        enableGraticule={true}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            },
-            {
-                id: 'gradient',
-                type: 'linearGradient',
-                colors: [
-                    {
-                        offset: 0,
-                        color: '#000'
-                    },
-                    {
-                        offset: 100,
-                        color: 'inherit'
-                    }
-                ]
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'CAN'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'CHN'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'ATA'
-                },
-                id: 'gradient'
-            }
-        ]}
-        legends={[
+        projectionScale={isDashboard? 40: 150}
+        projectionTranslation={isDashboard? [0.49,0.6]:[ 0.5, 0.5 ]}
+        projectionRotation={[ 0, 0, 0 ]}        
+        borderWidth={1.1}
+        borderColor="#fff"
+        
+        legends={
+            !isDashboard ? [
             {
                 anchor: 'bottom-left',
                 direction: 'column',
@@ -126,8 +72,8 @@ const GeoChart = () => {
                         }
                     }
                 ]
-            }
-        ]}
+            }] : undefined
+        }
     />
     );
 };
